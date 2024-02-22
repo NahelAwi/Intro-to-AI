dcc853094450a056c1cfc330e42db48b2b75b0b6
NahelAwi <awidatnahel22@gmail.com>
2024-02-22 13:23:10 +0200

    implemented BFS-G

--- HW1/Algorithms.py
+++ HW1/Algorithms.py
@@ -4,13 +4,51 @@ from DragonBallEnv import DragonBallEnv
 from typing import List, Tuple
 import heapdict
 
+class Node():
+    def __init__(self, state: Tuple, parent : 'Node' = None) -> None:
+        self.state = state #STATE _ (Position,Ball1,Ball2)
+        self.parent = parent
+        self.successors = []
+        self.actionsList = []
+        
+    def expand(self, d:dict) -> list['Node']:
+        if(d == None):
+            return []
+        for a in d.keys():
+            if(d[a] != (None, None, None)):# and d[a][0] != (None,False,False)):
+                NewNode = Node(d[a][0],self)
+                NewNode.actionsList.extend(self.actionsList)
+                NewNode.actionsList.append(a)
+                
+                self.successors.append(NewNode)
+        return self.successors
+        
 
 class BFSAgent():
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.Open = []
+        self.Close = []
+        self.nodesExpanded = 0
+        self.totalCost = 0
+        #self.actions = []
 
     def search(self, env: DragonBallEnv) -> Tuple[List[int], float, int]:
-        raise NotImplementedError
+        n = Node(env.get_initial_state())
+        if(env.is_final_state(n.state)):
+            return ([],0,1) #OR 0 for first node
+        self.Open.clear()
+        self.Close.clear()
+        self.Open.append(n)
+        while len(self.Open) != 0:
+            n = self.Open.pop()
+            self.Close.append(n.state)
+            for child in n.expand(env.succ(n.state)):
+                self.nodesExpanded += 1
+                if (child.state not in self.Close) and (child not in self.Open):
+                    if env.is_final_state(child.state):
+                        return (child.actionList,self.totalCost,self.nodesExpanded)
+                    self.Open.append(child)
+        return ([],-1,self.nodesExpanded)#Failure
 
 
 class WeightedAStarAgent():
