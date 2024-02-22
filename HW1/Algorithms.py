4da9958a6302629eb63147d3ec0f28594841f55b
NahelAwi <awidatnahel22@gmail.com>
2024-02-22 15:07:28 +0200

    question 2.1 Done

--- HW1/Algorithms.py
+++ HW1/Algorithms.py
@@ -8,7 +8,6 @@ class Node():
     def __init__(self, state: Tuple, parent : 'Node' = None) -> None:
         self.state = state #STATE _ (Position,Ball1,Ball2)
         self.parent = parent
-        #self.successors = []
         self.actionsList = []
         self.totalCost = 0
     
@@ -24,7 +23,7 @@ class Node():
             if(env.succ(self.state)[a] == (None,None,None)):
                 continue
             state , cost , termiated = env.step(a)
-            # if termiated and (state[1] == False or state[2] == False):
+            # if termiated and (state[1] == False or state[2] == False): #HERE WE DONT COUNT HOLES#
             #     continue
             NewNode = Node(state,self)
             NewNode.totalCost = self.totalCost + cost
@@ -50,15 +49,14 @@ class BFSAgent():
         self.Open.clear()
         self.Close.clear()
         self.Open.append(n)
-        self.nodesExpanded += 1
         while len(self.Open) != 0:
             n = self.Open.pop(0)
-            self.Close.append(n.state)  
+            self.Close.append(n.state)
+            self.nodesExpanded += 1  
             for child in n.expand(env):
                 if (child.state not in self.Close) and (child not in self.Open):
                     if env.is_final_state(child.state):
                         return (child.actionsList , child.totalCost ,self.nodesExpanded)
-                    self.nodesExpanded += 1
                     self.Open.append(child)
         return ([],-1,self.nodesExpanded)#Failure
 
