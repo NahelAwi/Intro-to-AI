a32418de1c0a3702033cf9d3e10e86b7b07e4c50
NahelAwi <awidatnahel22@gmail.com>
2024-02-22 14:58:30 +0200

    almost almost bfs-g

--- HW1/Algorithms.py
+++ HW1/Algorithms.py
@@ -8,28 +8,32 @@ class Node():
     def __init__(self, state: Tuple, parent : 'Node' = None) -> None:
         self.state = state #STATE _ (Position,Ball1,Ball2)
         self.parent = parent
-        self.successors = []
+        #self.successors = []
         self.actionsList = []
         self.totalCost = 0
+    
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
         
         
     def expand(self, env: DragonBallEnv) -> list['Node']:
         nA = 4
+        successors = []
         for a in range(nA):
             env.set_state_2(self.state)
             if(env.succ(self.state)[a] == (None,None,None)):
                 continue
             state , cost , termiated = env.step(a)
-            if termiated and (state[1] == False or state[2] == False):
-                continue
+            # if termiated and (state[1] == False or state[2] == False):
+            #     continue
             NewNode = Node(state,self)
             NewNode.totalCost = self.totalCost + cost
             NewNode.actionsList.extend(self.actionsList)
             NewNode.actionsList.append(a)
             
-            self.successors.append(NewNode)
+            successors.append(NewNode)
 
-        return self.successors
+        return successors
         
 
 class BFSAgent():
@@ -46,9 +50,10 @@ class BFSAgent():
         self.Open.clear()
         self.Close.clear()
         self.Open.append(n)
+        self.nodesExpanded += 1
         while len(self.Open) != 0:
             n = self.Open.pop(0)
-            self.Close.append(n.state)
+            self.Close.append(n.state)  
             for child in n.expand(env):
                 if (child.state not in self.Close) and (child not in self.Open):
                     if env.is_final_state(child.state):
