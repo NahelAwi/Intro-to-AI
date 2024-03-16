02a763e3a356356e51b96da6c8b1a6f2b0503640
NahelAwi <awidatnahel22@gmail.com>
2024-03-17 01:49:36 +0200

    Part 2 wet Done - time resource bound minimax implemented (Part 1 heuristic used Needs checking, Followed the psuedo code in the tutorial

--- HW2/AI_EX2/submission.py
+++ HW2/AI_EX2/submission.py
@@ -1,7 +1,8 @@
+from tkinter import NO
 from Agent import Agent, AgentGreedy
 from WarehouseEnv import WarehouseEnv, manhattan_distance
 import random
-
+import time
 
 # TODO: section a : 3
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
@@ -32,7 +33,42 @@ class AgentGreedyImproved(AgentGreedy):
 class AgentMinimax(Agent):
     # TODO: section b : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        operation,value = self.TRB_minimax(self,env,agent_id= agent_id,time_limit=time_limit,my_id=agent_id)
+        if(operation == None):
+                return AgentGreedyImproved().run_step(env,agent_id,time_limit)
+        return operation
+
+    def TRB_minimax(self, env: WarehouseEnv, agent_id , time_limit, my_id):
+        begin = time.time()
+        if(env.done() or time_limit <= 0.1):
+            return None,smart_heuristic(env,agent_id)
+        operators = env.get_legal_operators(agent_id)
+        children = [env.clone() for _ in operators]
+        if(agent_id == my_id):
+            curr_max_value = -float("inf")
+            curr_max_operation = None
+
+            for child, op in zip(children, operators):
+                child.apply_operator(agent_id, op)
+                end = time.time()
+                operation,value = self.TRB_minimax(child, env, (agent_id+1)%2, time_limit - (end-begin), my_id)
+                if(value > curr_max_value):
+                    curr_max_operation = operation
+                    curr_max_value = value
+            return curr_max_operation,curr_max_value
+        else:
+            curr_min_value = float("inf")
+            curr_min_operation = None
+
+            for child, op in zip(children, operators):
+                child.apply_operator(agent_id, op)
+                end = time.time()
+                operation,value = self.TRB_minimax(child, env, (agent_id+1)%2, time_limit - (end-begin), my_id)
+                if(value < curr_min_value):
+                    curr_min_operation = operation
+                    curr_min_value = value
+            return curr_min_operation,curr_min_value
+        
 
 
 class AgentAlphaBeta(Agent):
