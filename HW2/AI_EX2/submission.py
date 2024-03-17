fd8d60a6ee2bd047b3402d8958031542347cc004
NahelAwi <awidatnahel22@gmail.com>
2024-03-17 15:33:45 +0200

    AlphaBeta implementation

--- HW2/AI_EX2/submission.py
+++ HW2/AI_EX2/submission.py
@@ -7,23 +7,22 @@ import time
 # TODO: section a : 3
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
     robot = env.get_robot(robot_id)
-    tValue = max(robot.battery - manhattan_distance(robot.position, env.packages[0].position),
-                 #- manhattan_distance(env.packages[0].position, env.packages[0].destination )
+    tValue = max(robot.battery - manhattan_distance(robot.position, env.packages[0].position)
+                 - manhattan_distance(env.packages[0].position, env.packages[0].destination ),
                  robot.battery - manhattan_distance(robot.position, env.packages[1].position)
-                 #- manhattan_distance(env.packages[1].position, env.packages[1].destination )
-                 )
+                 - manhattan_distance(env.packages[1].position, env.packages[1].destination ))
     
     if(tValue >= 0):
         fvalue = tValue
     else:
         fvalue = max(-manhattan_distance(robot.position,env.charge_stations[0].position),
-                     -manhattan_distance(robot.position,env.charge_stations[1].position)) + robot.battery*2
-    sixpack = 0
-    deliver = 0
-    if (tValue >= 0 and robot.package != None):
-        return (robot.battery-manhattan_distance(robot.position, robot.package.destination) + robot.credit)*5
-
-    return fvalue + 7*robot.credit
+                     -manhattan_distance(robot.position,env.charge_stations[1].position))
+    
+    if(robot.package != None):
+        return fvalue + 4 - 0.5*manhattan_distance(robot.position, robot.package.destination)
+    else:
+        return fvalue + 2*robot.credit
+    
 
 class AgentGreedyImproved(AgentGreedy):
     def heuristic(self, env: WarehouseEnv, robot_id: int):
@@ -74,7 +73,51 @@ class AgentMinimax(Agent):
 class AgentAlphaBeta(Agent):
     # TODO: section c : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        operation,value = self.TRB_AlphaBeta_minimax(self,env, agent_id= agent_id, time_limit=time_limit, my_id=agent_id , Alpha=-float("inf"), Beta=float("inf"))
+        if(operation == None):
+                return AgentGreedyImproved().run_step(env,agent_id,time_limit)
+        return operation
+
+    def TRB_AlphaBeta_minimax(self, env: WarehouseEnv, agent_id , time_limit, my_id, Alpha, Beta):
+        begin = time.time()
+        if(env.done() or time_limit <= 0.1):
+            return None,smart_heuristic(env,agent_id)
+        operators = env.get_legal_operators(agent_id)
+        children = [env.clone() for _ in operators]
+        if(agent_id == my_id):
+            curr_max_value = -float("inf")
+            curr_max_operation = None
+
+            for child, op in zip(children, operators):
+                child.apply_operator(agent_id, op)
+                end = time.time()
+                operation,value = self.TRB_AlphaBeta_minimax(child, env, (agent_id+1)%2, time_limit - (end-begin), my_id, Alpha, Beta)
+                
+                if(value > curr_max_value):
+                    curr_max_operation = operation
+                    curr_max_value = value
+                
+                Alpha = max(Alpha, curr_max_value)
+                if( curr_max_value >= Beta):
+                    return None, float("inf")
+            return curr_max_operation,curr_max_value
+        else:
+            curr_min_value = float("inf")
+            curr_min_operation = None
+
+            for child, op in zip(children, operators):
+                child.apply_operator(agent_id, op)
+                end = time.time()
+                operation,value = self.TRB_AlphaBeta_minimax(child, env, (agent_id+1)%2, time_limit - (end-begin), my_id, Alpha, Beta)
+
+                if(value < curr_min_value):
+                    curr_min_operation = operation
+                    curr_min_value = value
+
+                Beta = min(Beta, curr_min_value)
+                if( curr_min_value <= Alpha):
+                    return None, -float("inf")
+            return curr_min_operation,curr_min_value
 
 
 class AgentExpectimax(Agent):
